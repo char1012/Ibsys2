a439af13aaa790654e108cf9bb180351ee15c1f3
char1012 <achristiansen1988@googlemail.com>
2013-12-02 18:12:26 +0100

    Neue Klasse angelegt für die Erstellung der Export-XML

---
+++ IBSYS2/IBSYS2/ExportXMLClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Data.OleDb;
+
+namespace IBSYS2
+{
+    class ExportXMLClass
+    {
+    }
+}

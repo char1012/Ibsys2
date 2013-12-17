979d16dba1f1a50fbd4fcc90d7e9162906a997f2
Jan Lauinger <lauinger.jan[at]googlemail.com>
2013-12-17 14:52:37 +0100

    MS - Tooltips passen genau übereinander IP, SB und PR

--- IBSYS2/IBSYS2/Sprachen.cs
+++ IBSYS2/IBSYS2/Sprachen.cs
@@ -101,7 +101,7 @@ namespace IBSYS2
 
         /*---------Sicherheitsbestand----------*/
         //Buttons
-        public static String DE_BTN_ETEILEBERECHNEN = "E-Teile Berechnen";
+        public static String DE_BTN_ETEILEBERECHNEN = "E-Teile berechnen";
         public static String DE_BTN_CONTINUE = "Weiter";
         public static String DE_BTN_BACK = "Zurück"; //Umlaut
 
@@ -116,7 +116,7 @@ namespace IBSYS2
 
         //Tooltip
         public static String DE_INFOP = "Bitte den Sicherheitsbestand eingeben, welcher für die P-Teile gehalten werden soll.";
-        public static String DE_INFOE = "- Diese Felder der Sicherheitsbestände für die E-Teile ist vor Berechnung der P-Teile nicht pflegbar. \n" + "- Das Ergbenis der Sicherheitsbestände der E-Teile wird vom System berechnet, können aber nach Bedarf händisch nachgefplegt werden. \n" + "- Um fortzufahren auf 'Fortfahren' klicken.";
+        public static String DE_INFOE = "- Diese Felder der Sicherheitsbestände für die E-Teile ist vor Berechnung der P-Teile nicht pflegbar.\n" + "- Das Ergbenis der Sicherheitsbestände der E-Teile wird vom System berechnet, können aber nach Bedarf händisch nachgefplegt werden.\n" + "- Um fortzufahren auf 'Fortfahren' klicken.";
 
         /*---------Ergebnis----------*/
         //Buttons
@@ -164,10 +164,10 @@ namespace IBSYS2
         public static String EN_LBL_IP_PERIODEX1 = "Period X+1";
         public static String EN_LBL_IP_PERIODEX2 = "Period X+2";
 
-        //InfoBox
-        public static String EN_IP_INFO = "First of all, select the current period, and then press the provided button to import the XML file. \nAfter that please enter their forecasts for the coming periods. \nYou can then continue editing";
-        public static String EN_IP_INFO_SCHRITT1 = "First, select the current period, and then press the provided button to import the XML file.";
-        public static String EN_IP_INFO_SCHRITT2 = "Now enter the forecasts for the next period.";
+        //InfoBox                          Wählen Sie als erstes die aktuelle Periode aus und betätigen Sie anschließend die bereitgestellte Schaltfläche zum Import der XML-Datei. \nIm Anschluss geben Sie bitte ihre Prognosen für die kommenden Perioden ein. \nAnschließend können Sie mit der Bearbeitung fortfahren.
+        public static String EN_IP_INFO = "First of all, select the current period, and then press the provided button to import the XML file.                                                                         \nAfter that please enter their forecasts for the coming periods.             \nYou can then continue editing                          ";
+        public static String EN_IP_INFO_SCHRITT1 = "First, select the current period, and then press the provided button to import the XML file.                                                                                 ";
+        public static String EN_IP_INFO_SCHRITT2 = "Now enter the forecasts for the next period.                           ";
 
         //ComboBox
         public static String EN_CB_IP_PERIODE_AUSWAEHLEN = "Select the period to edit";
@@ -213,7 +213,7 @@ namespace IBSYS2
         public static String EN_PR_GROUPBOX1 = "Production planning";
 
         //ToolTip
-        public static String EN_PR_INFO = " ";
+        public static String EN_PR_INFO = "Here you can overwrite the calculated production values. \nTo check the production of the E-Parts, click on the button 'E-Parts'.                     ";
         /*---------Produktion E-Teile----------*/
         public static String EN_PRE_GB_ETEILE = "Production of own products";
 
@@ -233,9 +233,9 @@ namespace IBSYS2
         public static String EN_LABEL4 = "You can overwrite the calculated values.";
         public static String EN_LABLE9 = "* E-Items are summed for further calcualtion.";
 
-        //Tooltip
-        public static String EN_INFOP = "Please enter the Safty-Stock for the P-Items.";
-        public static String EN_INFOE = "- These fields of safety stocks for the E-Items is not maintainable before calculation of the P-Items. \n "+" - The result showing the security holdings of the E-Items is calculated by the system, but can be changed manually as needed. \n "+" - to continue on 'Continue' button.";
+        //Tooltip                        
+        public static String EN_INFOP = "Please enter the Safty-Stock for the P-Items.                                       ";
+        public static String EN_INFOE = "- These fields of safety stocks for the E-Items is not maintainable before calculation of the P-Items.\n " + " - The result showing the security holdings of the E-Items is calculated by the system, but can be changed manually as needed.                \n " + " - to continue click on 'Continue' button.";
         
         /*---------Ergebnis----------*/
         //Buttons
